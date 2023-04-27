2ff4f4f46ed3349be88fa6327b5fbac3bdc936f2
Inaê Rosa <inaedb.rosa@gmail.com>
2023-04-26 21:42:44 -0300

    feat: save user

---
+++ src/controllers/userController.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const payloads = require('../../payloads.json')
+module.exports = {
+    create: (req, res) => {
+        const {name, email} = req.body;
+        try{
+            const user = {name, email}
+            payloads.push(user)
+            fs.writeFileSync('../../payloads.txt', payloads.toString())
+            res.status(201).send(user)
+        }catch(err) {
+            res.status(500).json(err.message)
+        }
+    },
+
+
+}
