c011e93f152af99dc47703baae1256e598da9137
Inaê Rosa <inaedb.rosa@gmail.com>
2023-05-04 19:06:13 -0300

    feat: remove user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,7 +6,7 @@ module.exports = {
         try{
             const user = {name, email}
             payloads.push(user)
-            fs.writeFileSync('../../payloads.txt', payloads.toString())
+            fs.writeFileSync('./payloads.json', JSON.stringify(payloads))
             res.status(201).send(user)
         }catch(err) {
             res.status(500).json(err.message)
@@ -14,6 +14,17 @@ module.exports = {
     },
     list: (req, res) => {
         res.status(200).json(payloads)
+    },
+    remove: async(req, res) => {
+        try{
+            const {name} = req.body;
+            const indexUser = payloads.findIndex(user => user.user === name)
+            payloads.splice(indexUser, 1);
+            fs.writeFileSync('./payloads.json', JSON.stringify(payloads))
+            res.status(204).json('Removido!');
+        }catch(err){
+            res.status(500).json(err.message)
+        }
     }
 
 }
