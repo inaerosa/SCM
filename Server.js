661c46d8bca0ec2ac2fcf0c30fc6ba3510ac2e42
Inaê Rosa <inaedb.rosa@gmail.com>
2023-06-27 21:57:32 -0300

    feat: create index endpoint

--- Server.js
+++ Server.js
@@ -6,6 +6,10 @@ const router = require('./src/routes/userRoutes')
 app.use(express.json())
 app.use('/api/users', router)
 
+app.get('/', (req, res) => {
+    res.send('Ola, eu sou o trabalho do roger')
+})
+
 app.listen(3000, () => {
     console.log(`Running at 3000`)
 })
