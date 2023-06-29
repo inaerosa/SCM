9aa7731281bfeef41f9cfde3f74eb1d3dbcb2d56
Inaê Rosa <inaedb.rosa@gmail.com>
2023-06-29 20:27:47 -0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const app = require('express')();
 require('dotenv').config();
+const swaggerDocument = require('./swagger.json');
+const swaggerUi = require('swagger-ui-express');
+const cors = require('cors')
 const router = require('./src/routes/userRoutes')
-
 app.use(express.json())
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    res.header("Access-Control-Allow-Methods", 'GET,PATCH,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
 app.use('/api/users', router)
 
-app.get('/', (req, res) => {
-    res.send('Ola, eu sou o trabalho do roger')
-})
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+
 
 app.listen(3000, () => {
     console.log(`Running at 3000`)
